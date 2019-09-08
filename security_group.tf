4bf3b61eb6e72a1df3c313cf32302980a5677261
Lou Bichard <contact@louisjohnbichard.co.uk>
2019-09-08 14:33:29 +0100

    :sparkles: Consolidated security group variables

--- security_group.tf
+++ security_group.tf
@@ -3,7 +3,7 @@ resource "aws_security_group_rule" "allow_tcp_ingress" {
   from_port         = 80
   to_port           = 80
   protocol          = "TCP"
-  cidr_blocks       = ["10.0.0.0/27"]
+  cidr_blocks       = ["${var.subnet_1_public}"]
   security_group_id = "${aws_security_group.allow_http.id}"
 }
 
@@ -12,7 +12,7 @@ resource "aws_security_group_rule" "allow_all_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = -1
-  cidr_blocks       = ["10.0.0.0/27"]
+  cidr_blocks       = ["${var.subnet_1_public}"]
   security_group_id = "${aws_security_group.allow_http.id}"
 }
 
